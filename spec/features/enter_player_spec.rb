c817e698c3cb8c9cc8297a9b577794772984a70f
shadz22 <shadz22@yahoo.co.uk>
2020-01-09 17:20:38 +0000

    Test helpers

--- spec/features/enter_player_spec.rb
+++ spec/features/enter_player_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Enter player' do
   scenario 'lets players to submit names in a form and submit the form' do
-    visit ('/')
-    fill_in :player_1_name, with: "Shadi"
-    fill_in :player_2_name, with: "Marcello"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Shadi vs. Marcello")
   end
 end
--- spec/features/viewing_hit_points_spec.rb
+++ spec/features/viewing_hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Viewign hit points' do
   scenario "see player 2's hit points" do
-    visit ('/')
-    fill_in :player_1_name, with: "Shadi"
-    fill_in :player_2_name, with: "Marcello"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Marcello: 60HP")
   end
 end
